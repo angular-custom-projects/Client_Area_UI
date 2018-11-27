f112b886249f2e6f8814adc02face9d36b28e691
Parween <parween.kelappan@opcbiz.com>
2018-11-27 12:58:21 +0800

    MAU-13 Client Profile - Personal Details
    - Add profile sidebar menu link routes
    - Enhance page design

---
+++ src/app/profile/trading-knowledge/trading-knowledge.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trading-knowledge',
+  templateUrl: './trading-knowledge.component.html',
+  styleUrls: ['./trading-knowledge.component.scss']
+})
+export class TradingKnowledgeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
