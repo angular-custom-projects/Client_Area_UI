b4150ff58a2a501f963d0d05d9083260a1c77e57
Adam Morsi <adam.morsi@yahoo.com>
2018-11-29 10:58:17 +0200

    Add bank details component

--- src/app/profile/profile-routing.module.ts
+++ src/app/profile/profile-routing.module.ts
@@ -3,11 +3,13 @@ import {Routes, RouterModule} from '@angular/router';
 
 import {ProfileDetailsComponent} from './profile-details/profile-details.component';
 import {ProfileComponent} from './profile/profile.component';
+import {BankDetailsComponent} from "./bank-details/bank-details.component";
 
 const routes: Routes = [
     {
         path: '', component: ProfileComponent, children: [
-            {path: '', component: ProfileDetailsComponent, pathMatch: 'full'}
+            {path: '', component: ProfileDetailsComponent, pathMatch: 'full'},
+            {path: 'bank-info', component: BankDetailsComponent}
         ]
     }
 ];
--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -1,6 +1,7 @@
 // this module is used for the profile feature
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {FormsModule} from "@angular/forms";
 import {AngularFontAwesomeModule} from 'angular-font-awesome';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {MaterialModule} from '../material/material.module';
@@ -9,17 +10,19 @@ import {ProfileRoutingModule} from './profile-routing.module';
 import {ProfileDetailsComponent} from './profile-details/profile-details.component';
 import {ProfileComponent} from './profile/profile.component';
 import {SharedModule} from '../shared/shared.module';
+import {BankDetailsComponent} from './bank-details/bank-details.component';
 
 @NgModule({
     imports: [
         CommonModule,
+        FormsModule,
         ProfileRoutingModule,
         AngularFontAwesomeModule,
         FlexLayoutModule,
         MaterialModule,
         SharedModule
     ],
-    declarations: [ProfileDetailsComponent, ProfileComponent]
+    declarations: [ProfileDetailsComponent, ProfileComponent, BankDetailsComponent]
 })
 export class ProfileModule {
 }
